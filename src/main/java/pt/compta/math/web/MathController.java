791f25e2f676484cad07897d01b5e46c56e8a857
Icefyred <dcastilho001@hotmail.com>
2019-03-30 17:28:08 +0000

    Added extra logic to the MathController.java file

--- src/main/java/pt/compta/math/web/MathController.java
+++ src/main/java/pt/compta/math/web/MathController.java
@@ -6,21 +6,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pt.compta.tools.fourthmaven.MathUtils;
 
-
 @RestController
 public class MathController {
 
-	@RequestMapping(value = "/calculate", method = RequestMethod.GET)
-	public String calculate(@RequestParam("op") String typeOfOperator, @RequestParam("val1") String firstNumberValue, @RequestParam("val2") String secondNumberValue) {
-		
+	@RequestMapping(value = "/math/sum", method = RequestMethod.GET)
+	public String calculateSum(@RequestParam("val1") String firstNumberValue,
+			@RequestParam("val2") String secondNumberValue) {
+
 		String resultFromOperation = "";
-		
-		if(typeOfOperator.equals("subtract")) {
-			resultFromOperation += MathUtils.subtractOfTwoNumbers(Integer.parseInt(firstNumberValue), Integer.parseInt(secondNumberValue));
-		}
-		else if(typeOfOperator.equals("sum")) {
-			resultFromOperation += MathUtils.sumOfTwoNumbers(Integer.parseInt(firstNumberValue), Integer.parseInt(secondNumberValue));
-		}
+
+		resultFromOperation += MathUtils.sumOfTwoNumbers(Integer.parseInt(firstNumberValue),
+				Integer.parseInt(secondNumberValue));
+		return "Result:  " + resultFromOperation;
+	}
+
+	@RequestMapping(value = "/math/subtract", method = RequestMethod.GET)
+	public String calculateSubtract(@RequestParam("val1") String firstNumberValue,
+			@RequestParam("val2") String secondNumberValue) {
+
+		String resultFromOperation = "";
+
+		resultFromOperation += MathUtils.subtractOfTwoNumbers(Integer.parseInt(firstNumberValue),
+				Integer.parseInt(secondNumberValue));
+
 		return "Result:  " + resultFromOperation;
 	}
 
